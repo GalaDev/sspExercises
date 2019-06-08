febe5427b7fef6bf060027a70155511cb599cd36
Narendra Gala <livelifebrands@gmail.com>
2019-06-07 22:30:13 -0700

    Add methods with each for array and objects

--- HOF/hof2.js
+++ HOF/hof2.js
@@ -11,4 +11,29 @@ function indexedExponentials(numbers) {
   });
   return exArr;
 }
+
 console.log(indexedExponentials([2, 5, 7, 4]));
+
+function evenIndexedOddNumbers(numbers) {
+  const oddNum = [];
+  each(numbers, (num, i) => {
+    if(num % 2 !== 0 && i % 2 === 0) oddNum.push(num);
+  });
+  return oddNum;
+}
+console.log(evenIndexedOddNumbers([1, 3, 3, 4, 7, 10])); // => [1, 3, 7]
+
+function evenIndexedEvenLengths(strings) {
+  const acc = [];
+
+  each(strings, (el, i) => {
+    if(el.length % 2 === 0 && i % 2 === 0) acc.push(el);
+  });
+
+  return acc;
+}
+console.log(evenIndexedEvenLengths(["lion", "monkey", "aardvaark", "cat", "doge"]));
+// => ["lion", "doge"]
+console.log(evenIndexedEvenLengths(["red", "green", "purple", "blue", "yellow"]));
+// => ["purple", "yellow"]
+
