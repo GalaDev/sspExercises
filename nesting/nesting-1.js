fcda04ddc66e961db710b92dc0d7470e32c2c8c0
Narendra Gala <livelifebrands@gmail.com>
2019-06-07 21:19:01 -0700

    All functions working

--- nesting/nesting-1.js
+++ nesting/nesting-1.js
@@ -1,3 +1,5 @@
+const log = console.log;
+
 var arrayA = [
   "hello",
   "world",
@@ -57,3 +59,125 @@ var bigBox = {
 var volumeOfSmallBoxA = bigBox.smallBoxA.volume;
 var weightOfSmallBoxB = bigBox.smallBoxB.weight;
 var isSmallBoxCFull = bigBox.smallBoxC.full;
+
+const randomInt = num => {
+  return Math.ceil(Math.random() * (num - 1) + 1);
+};
+
+const boxMaker = () => {
+  let box = {
+    length: randomInt(5),
+    width: randomInt(10),
+    height: randomInt(10),
+    contents: []
+  }
+
+  return box;
+};
+
+var sampleBox1 = boxMaker(); //creates a "box" object and assigns it to "sampleBox"
+var sampleBox2 = boxMaker(); //creates another "box" object, and is completely independent of the 1st function invocation in "sampleBox"
+const warehouse = [];
+
+const orderBoxes = (num) => { 
+  for(let i = 0; i < num; i++){
+    warehouse.push(boxMaker())
+  }
+  return warehouse;
+};
+
+orderBoxes(10)
+
+const getVolume = (box) => {
+  return box.length * box.width * box.height;
+};
+
+warehouse.forEach((box) => {
+  box.contents.push('saftey manual');
+  box.contents.push('mothballs');
+});
+
+var youDontNeedToWorryAboutAnythingInHere = function() {
+    var NameArray = ['Noel Plain', 'Steffanie Plain', 'Hester Sanderfur', 'Retta Plain', 'Idella Saur','Shila Plain', "Marybeth Peavler", "Chadwick Jin", "Jerold Chauez", "Roselle Plain", "Doria Figgins", "Seth Arvizu", 'Gloria Wyche', "Melodi Plain", "Robin Shackleford", "Jack Plain", "Marina Prost", "Melvina Plain", "Ryan Plemons", "Janis Ohare", "Assata Shakur", "Kanye West", "Jay-Z", "Mariah Carey", "Bianca Gandolfo", "Cedric the Entertainer", "Chloe Plain"];
+    var JobArray = ['Mortician', 'Broadcaster', 'Craftsperson', 'Engineer', 'Interior designer', 'Nun'];
+    var CityArray = ["Scottsdale, Arizona", "Oakland, California", "Stockton, California", "New York, NY"];
+    var emptyNetwork = [];
+
+    function createNetwork(collection){
+
+        for (var i = 0; i < NameArray.length; i++) {
+            var randomJob = JobArray[Math.floor(Math.random() * JobArray.length)];
+            var randomCity = CityArray[Math.floor(Math.random() * CityArray.length)];
+
+            collection.push(createNewUser(NameArray[i], randomJob, randomCity));
+        }
+
+        return collection
+    }
+
+    return createNetwork(emptyNetwork);
+
+}
+
+function createNewUser(username, job, city){
+    var newUser = {
+        username: username,
+        job: job,
+        city: city,
+        friends: [],
+        family: [],
+        coworkers: [],
+    };
+
+    return newUser;
+
+};
+// creating the user you're going to be adding to
+var joeyProfile = createNewUser('Joey Plain', 'Engineer', 'Oakland, California');
+// creating the network collection you're going to be pulling
+var allUsers = youDontNeedToWorryAboutAnythingInHere();
+// check the console to see what these look like
+
+
+console.dir(allUsers);
+log(joeyProfile);
+
+// YOUR CODE HERE
+allUsers.forEach((user) => {
+  if(user.city === joeyProfile.city){
+    joeyProfile.friends.push(user.username);
+  }
+});
+log(joeyProfile);
+
+
+allUsers.forEach((user) => {
+  if(user.job === joeyProfile.job){
+    joeyProfile.coworkers.push(user.username);
+  }
+});
+log(joeyProfile);
+
+allUsers.forEach((user) => {
+  if(user.username.includes('Plain')){
+    joeyProfile.family.push(user.username);
+  }
+});
+log(joeyProfile);
+
+joeyProfile.family.forEach((famName, i) => {
+  joeyProfile.friends.forEach((friName, j) => {
+    if(famName === friName) joeyProfile.friends.splice(j, 1);
+  });
+});
+log(joeyProfile);
+
+joeyProfile.family.forEach((famName, i) => {
+  joeyProfile.coworkers.forEach((coName, k) => {
+    if(famName === coName) joeyProfile.coworkers.splice(k, 1);
+  });
+});
+log(joeyProfile);
+
+
+
